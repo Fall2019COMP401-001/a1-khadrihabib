c273d92874ed469bc4065fbde72556bf42805f1f
habibkhadri <habibkhadri@habibs-mbp.wireless-1x.unc.edu>
2019-09-01 15:24:19 -0400

    final adept

--- src/a1/src/a1/A1Adept.java
+++ src/a1/src/a1/A1Adept.java
@@ -9,55 +9,57 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		int count = scan.nextInt(); 
+		int count = scan.nextInt(); // initial count
 		String[] groceryitem = new String[count];
 		double[] tprice = new double[count]; // an array of doubles (prices)
-		for(int i = 0; i < count; i++) { // for loop for the customers
-			groceryitem[i] = scan.next();
-			tprice[i] = scan.nextDouble();
+		for (int i = 0; i < count; i++) { // for loop for the customers
+			groceryitem[i] = scan.next(); // this will scan through the list of grocery items
+			tprice[i] = scan.nextDouble(); // will scan through the price of each item
 		}
 		int object = scan.nextInt();
 		String[] fnames = new String[object];
 		String[] lnames = new String[object];
-		Double[] totalexp = new Double [object];
+		Double[] totalexp = new Double[object];
 		for (int zero = 0; zero < object; zero++) {
 			totalexp[zero] = 0.0;
-		}	
-		for(int j = 0; j < object; j++) { // for loop for the customers
-			fnames[j] = scan.next();
-			lnames[j] = scan.next();
+		}
+		for (int j = 0; j < object; j++) { // for loop for the customers
+			fnames[j] = scan.next(); // scans for the first names
+			lnames[j] = scan.next(); // scans for the last names 
 			int number = scan.nextInt();
-			
-		for(int n=0; n < number; n++) {
-			int groceryquantity = scan.nextInt();
-			String groceries = scan.next();
-			
-			int index = 0;
-			for (int h=0; h<count; h++) {
-				if (groceries.equals(groceryitem[h])) {
-					index = h;
+
+			for (int n = 0; n < number; n++) {
+				int groceryquantity = scan.nextInt();
+				String groceries = scan.next();
+
+				int index = 0;
+				for (int h = 0; h < count; h++) {
+					if (groceries.equals(groceryitem[h])) {
+						index = h;
 					}
 				}
-			totalexp[j] += tprice[index] * groceryquantity;
+				totalexp[j] += tprice[index] * groceryquantity;
 			}
 		}
-		int biggest = 0;
-		int smallest = 0; 
-		double sum = 0;
-		float avg = 0;
-			for(int g = 0; g < totalexp.length; g++) {
-				if (totalexp[g] > totalexp[biggest]) {
-					biggest = g; 
-				}
-				if(totalexp[g] < totalexp[smallest]) {
-					smallest = g;
-				}
-			sum += totalexp[g]; 
+		int biggest = 0; // biggest int 
+		int smallest = 0; // smallest int 
+		double sum = 0; // add all values to find the average 
+		float avg = 0; // research float function 
+		for (int g = 0; g < totalexp.length; g++) {
+			if (totalexp[g] > totalexp[biggest]) {
+				biggest = g; // assign variable for biggest 
+			}
+			if (totalexp[g] < totalexp[smallest]) {
+				smallest = g; // assign variable for smallest 
+			}
+			sum += totalexp[g]; // sum of all values 
 		}
-			avg = (float)((biggest + smallest)/2);
-			
-		System.out.println("Biggest: " + fnames[biggest] + " " + lnames[biggest] + " (" +  String.format("%.2f", totalexp[biggest]) + ")");
-		System.out.println("Smallest: " + fnames[smallest] + " " + lnames[smallest] + " (" +  String.format("%.2f", totalexp[smallest]) + ")");
-		System.out.println("Average: " +  String.format("%.2f", (sum/totalexp.length)));
-		} 
+		avg = (float) ((biggest + smallest) / 2); // function to obtain the average
+
+		System.out.println("Biggest: " + fnames[biggest] + " " + lnames[biggest] + " ("
+				+ String.format("%.2f", totalexp[biggest]) + ")");
+		System.out.println("Smallest: " + fnames[smallest] + " " + lnames[smallest] + " ("
+				+ String.format("%.2f", totalexp[smallest]) + ")");
+		System.out.println("Average: " + String.format("%.2f", (sum / totalexp.length)));
+	}
 }
